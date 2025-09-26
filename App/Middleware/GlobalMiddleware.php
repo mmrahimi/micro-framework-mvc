a030d71275cded33a2b5c65a60a348494c3dcb6e
Mohammad Mahdi Rahimi <rahimi1.mohammadmahdi@gmail.com>
2025-09-26 18:21:14 +0330

    Add $_GET sanitizer in GlobalMiddleware

--- App/Middleware/GlobalMiddleware.php
+++ App/Middleware/GlobalMiddleware.php
@@ -6,7 +6,16 @@ use App\Middleware\Contract\MiddlewareContract;
 
 class GlobalMiddleware Implements MiddlewareContract
 {
-    public function handle()
+    public function handle(): void
     {
+        $this->sanitizeGetParams();
+    }
+
+    private function sanitizeGetParams(): void
+    {
+        foreach ($_GET as $key => $value)
+        {
+            $_GET[$key] = filter_var(htmlspecialchars($value), FILTER_SANITIZE_STRING);
+        }
     }
 }
